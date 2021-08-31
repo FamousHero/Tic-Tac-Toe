ec1ad0d0cc92bb89c35cd835c8473c229060769d
FamousHero <33110962+FamousHero@users.noreply.github.com>
2021-08-31 01:32:05 -0700

    Allowed rematch removed extra includes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include "TicTacToe.hpp"
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
 #include <iostream>
 #include <string>
 int main(){
-
-    TicTacToe* game =  new TicTacToe();
-    game->run();
-    delete game;
+    bool is_running = true;
+    std::string newGame;
+    while(is_running){
+        newGame = "";
+        TicTacToe* game =  new TicTacToe();
+        game->run();
+        delete game;
+        while(newGame != "Yes" && newGame != "No")
+        {
+            std::cout << "New Game? (Yes/No)" <<std::endl;
+            std::cin >> newGame;
+        }
+        if(newGame == "No")
+            is_running = false;
+    }
+    std::cout << "Goodbye...\n\n\n";
+    system("pause");
     return 0;
 
   
